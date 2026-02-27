ffc62c991b187b9748bb800d2f40c20b096517fb
Ruby <nhthuan23@clc.fitus.edu.vn>
2026-02-27 14:45:12 +0700

    feat: Create ViewModels and Views with example page. Using navigation bar from toolkits

---
+++ UI/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace UI.ViewModels
+{
+    public partial class SettingsViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string _greetingText = "Hello! MVVM đã hoạt động thành công!";
+    }
+}
