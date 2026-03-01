4cb0b38d0b4e10a1095ad31fa71eca201e3ef0ce
Pipi1225 <dghuy6625@gmail.com>
2026-03-01 17:12:55 +0700

    feat: setup HotChocolate GraphQL server & migrate DB config from UI to API

---
+++ MyShop.Api/GraphQL/Query.cs
@@ -0,0 +1,19 @@
+﻿using Core;
+using Core.Data;
+using Core.Models;
+using HotChocolate;
+
+namespace MyShop.Api.GraphQL;
+
+public class Query
+{
+    public IQueryable<Category> GetCategories([Service] AppDbContext context)
+    {
+        return context.Categories;
+    }
+
+    public IQueryable<User> GetUsers([Service] AppDbContext context)
+    {
+        return context.Users;
+    }
+}
