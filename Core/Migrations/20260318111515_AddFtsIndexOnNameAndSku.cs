01d9ed5ac075b7f9c0c95e5305e4f39bbffd41e5
Pipi1225 <dghuy6625@gmail.com>
2026-03-18 19:24:41 +0700

    feat: GIN Index to improve FTS's performance

---
+++ Core/Migrations/20260318111515_AddFtsIndexOnNameAndSku.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFtsIndexOnNameAndSku : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+            CREATE INDEX ""IX_Products_Name_SKU_FTS""
+            ON ""Products""
+            USING GIN (to_tsvector('simple', coalesce(""Name"", '') || ' ' || coalesce(""SKU"", '')));
+        ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP INDEX \"IX_Products_Name_SKU_FTS\";");
+        }
+    }
+}
