476024743081389673c0e453bc5c475de53e3c7d
Ruby <nhthuan23@clc.fitus.edu.vn>
2026-03-19 13:41:18 +0700

    feat: Implement OrderList Page backend

---
+++ Api/GraphQL/Queries/OrderQueries.cs
@@ -0,0 +1,32 @@
+﻿using Core.Data;
+using Core.Models;
+using HotChocolate;
+using HotChocolate.Types;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+
+namespace Api.GraphQL.Queries;
+
+[ExtendObjectType(typeof(Query))]
+internal class OrderQueries
+{
+    [UsePaging(DefaultPageSize = 20)]
+    [UseSorting]
+    public IQueryable<Order> GetOrders([Service] AppDbContext context)
+    {
+        var query = context.Orders.AsQueryable();
+
+        query = query.OrderByDescending(p => p.OrderDate).ThenBy(p => p.Id);
+
+        return query;
+    }
+
+    [UseSingleOrDefault]
+    [UseProjection]
+    public IQueryable<Order> GetOrderById(Guid id, [Service] AppDbContext dbContext)
+    { 
+        return dbContext.Orders.Where(p => p.Id == id);
+    }
+}
