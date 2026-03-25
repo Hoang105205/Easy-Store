f01999ff21379a0851a8038ba107254e2266b054
Pipi1225 <dghuy6625@gmail.com>
2026-03-25 16:37:47 +0700

    update: add MinimumStockQuantity to Product model

---
+++ Core/Migrations/20260325091209_AddMinimumStockToProduct.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMinimumStockToProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MinimumStockQuantity",
+                table: "Products",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MinimumStockQuantity",
+                table: "Products");
+        }
+    }
+}
