2f879d2b2a09fa420574b627c319f74ac00e39ad
Ruby <nhthuan23@clc.fitus.edu.vn>
2026-03-26 23:47:01 +0700

    refactor: apply strict MVVM to OrderDetailPage, using relay command.

--- UI/Views/Orders/OrderDetailPage.xaml.cs
+++ UI/Views/Orders/OrderDetailPage.xaml.cs
@@ -1,4 +1,3 @@
-using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Navigation;
 using System;
@@ -15,64 +14,28 @@ namespace UI.Views.Orders
         {
             this.InitializeComponent();
             ViewModel = App.Current.Services.GetRequiredService<OrderDetailPageViewModel>();
+
+            this.Loaded += (s, e) =>
+            {
+                ViewModel.XamlRoot = this.XamlRoot;
+            };
+
+            ViewModel.NavigateBackAction = () =>
+            {
+                if (Frame.CanGoBack)
+                {
+                    Frame.GoBack();
+                }
+            };
         }
 
         protected override async void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
+
             if (e.Parameter is Guid orderId)
             {
                 await ViewModel.LoadOrderAsync(orderId);
-            }
-        }
-
-        private void BtnBack_Click(object sender, RoutedEventArgs e)
-        {
-            if (Frame.CanGoBack)
-            {
-                Frame.GoBack();
-            }
-        }
-
-        private async void BtnPay_Click(object sender, RoutedEventArgs e)
-        {
-            ContentDialog dialog = new ContentDialog
-            {
-                Title = "Xác nhận thanh toán",
-                Content = "Bạn có chắc chắn muốn xác nhận thanh toán cho đơn hàng này?",
-                PrimaryButtonText = "Thanh toán",
-                CloseButtonText = "Hủy",
-                XamlRoot = this.XamlRoot
-            };
-
-            var result = await dialog.ShowAsync();
-            if (result == ContentDialogResult.Primary)
-            {
-                await ViewModel.PayOrderAsync();
-            }
-        }
-
-        private async void BtnDelete_Click(object sender, RoutedEventArgs e)
-        {
-            ContentDialog dialog = new ContentDialog
-            {
-                Title = "Xác nhận xóa",
-                Content = "Hành động này không thể hoàn tác. Bạn có chắc chắn muốn xóa đơn hàng này?",
-                PrimaryButtonText = "Xóa",
-                CloseButtonText = "Hủy",
-                DefaultButton = ContentDialogButton.Close,
-                XamlRoot = this.XamlRoot
-            };
-
-            var result = await dialog.ShowAsync();
-            if (result == ContentDialogResult.Primary)
-            {
-                bool isDeleted = await ViewModel.DeleteOrderAsync();
-                if (isDeleted && Frame.CanGoBack)
-                {
-                    // Trở về trang trước sau khi xóa thành công
-                    Frame.GoBack();
-                }
             }
         }
     }
