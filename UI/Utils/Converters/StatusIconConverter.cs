59bb551c2c3d2a4a571a049e5020e21465add87c
Pipi1225 <dghuy6625@gmail.com>
2026-04-02 14:55:58 +0700

    feat: enhance UI layout to be more analytical

--- UI/Utils/Converters/StatusIconConverter.cs
+++ UI/Utils/Converters/StatusIconConverter.cs
@@ -9,6 +9,16 @@ public class StatusIconConverter : IValueConverter
 {
     public object Convert(object value, Type targetType, object parameter, string language)
     {
+        if (value is double percentage)
+        {
+            if (percentage > 0)
+                return "\uE70E"; // Mũi tên hướng lên (ChevronUp)
+            if (percentage < 0)
+                return "\uE70D"; // Mũi tên hướng xuống (ChevronDown)
+
+            return "\uE73E"; // Dấu gạch ngang nếu bằng 0 (ChromeMinimize)
+        }
+
         var status = value?.ToString();
         if (status == "Completed" || status == "Hoàn thành")
             return "\uE73E"; // Mã Unicode của dấu Check (Hoàn thành)
