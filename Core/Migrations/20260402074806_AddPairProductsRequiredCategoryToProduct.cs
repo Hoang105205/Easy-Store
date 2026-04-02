fc8e49bf1c88a0a0e0ea7b3534f5d09c46d95705
Pipi1225 <dghuy6625@gmail.com>
2026-04-02 14:50:15 +0700

    update: add list pairProducts & make Category required for Product Schema

---
+++ Core/Migrations/20260402074806_AddPairProductsRequiredCategoryToProduct.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPairProductsRequiredCategoryToProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ProductId",
+                table: "Products",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_ProductId",
+                table: "Products",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Products_ProductId",
+                table: "Products",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Products_ProductId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_ProductId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "ProductId",
+                table: "Products");
+        }
+    }
+}
